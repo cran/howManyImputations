c21c2396611c86a935780b417079ac651e2f4035
Josh Errickson <jerrick@umich.edu>
2022-05-31 21:20:16 +0000

    version 0.2.3

--- NEWS.md
+++ NEWS.md
@@ -1,15 +1,11 @@
+# howManyImputations 0.2.2.9001
+
+- Because this package is just a thin wrapper around von Hippel (2020)'s work,
+  added citation to that paper rather than this package.
+- Added function `howManyImputations()` with a warning to direct users to the
+  proper `how_many_imputations()`.
+- Fixes to documentation.
+
 # howManyImputations 0.2.2
 
-Additional metadata tweaks for CRAN.
-
-# howManyImputations 0.2.1
-
-A few minor tweaks to metadata and documentation to pass CRAN checks.
-
-# howManyImputations 0.2.0
-
-First version submitted to CRAN. Contains mostly documentation cleanups.
-
-# howManyImputations 0.1.0
-
-Initial release.
+First release on CRAN.
--- R/how_many_imputations.R
+++ R/how_many_imputations.R
@@ -1,30 +1,28 @@
-#' How Many Imputations?
-#'
-#' Implements two-stage "how_many_imputations" from von Hippel (2018)
+#' Implements two-stage "how_many_imputations" from von Hippel (2020)
 #'
 #' The old advice of 5-10 imputations is sufficient for a point estimate (e.g.
 #' an estimated coefficient), but not for estimates of standard errors (and
 #' consequently, hypothesis tests or confidence intervals).
 #'
-#' von Hippel (2018) provides a way to calculate the number of imputations
+#' von Hippel (2020) provides a way to calculate the number of imputations
 #' needed to have consistent estimates of the standard error. To do so requires
 #' an estimate of the Fraction of Missing Information (FMI) which can only be
 #' obtained after running some number of imputations. Therefore, von Hippel
-#' (2018) recommends the following procedure:
+#' (2020) recommends the following procedure:
 #'
 #' \enumerate{
 #'   \item Carry out a limited number of imputations to enable estimation of the
-#'   FMI. von Hippel (2018) recommends 20 imputations.
-#'   \item Use this function, \code{howManyImputations}, to calculate how many
+#'   FMI. von Hippel (2020) recommends 20 imputations.
+#'   \item Use this function, \code{how_many_imputations()}, to calculate how many
 #'   total imputations you will need.
 #'   \item If the number of total imputations you will need is larger than your
 #'   initial batch of 20, run additional imputations.
 #' }
 #'
 #' @param model Either a \code{mira} object (created by running a model on a
-#'   data set which was imputed using \code{mice}) or a \code{mipo} object
+#'   data set which was imputed using \code{mice::mice()}) or a \code{mipo} object
 #'   (creating by running \code{pool()} on a \code{mira} object), or any object
-#'   which can be converted to \code{mira} via \code{as.mira}.
+#'   which can be converted to \code{mira} via \code{as.mira()}.
 #' @param cv Desired precision of standard errors. Default to .05. If the data
 #'   were re-imputed, the estimated standard errors would differ by no more than
 #'   this amount.
@@ -36,7 +34,7 @@
 #' @importFrom stats plogis qlogis qnorm
 #' @importFrom mice mice as.mira pool
 #'
-#' @references Von Hippel, Paul T. "How many imputations do you need? A two-stage calculation using a quadratic rule." Sociological Methods & Research 49.3 (2020): 699-718.
+#' @references von Hippel, Paul T. "How Many Imputations Do You Need? A Two-stage Calculation Using a Quadratic Rule." Sociological Methods & Research 49.3 (2020): 699-718.
 #'
 #' @examples
 #' data(airquality)
@@ -74,4 +72,18 @@ how_many_imputations <- function(model,
   fmiu <- plogis(qlogis(fmi) + z*sqrt(2/model$m))
 
   ceiling(1 + 1/2*(fmiu/cv)^2)
+}
+
+#' Improper function name, use \code{how_many_imputations()} instead
+#'
+#' @param model See \code{how_many_imputations()}
+#' @param cv See \code{how_many_imputations()}
+#' @param alpha See \code{how_many_imputations()}
+#' @return See \code{how_many_imputations()}
+#' @export
+howManyImputations <- function(model,
+                               cv = .05,
+                               alpha = .05) {
+  warning("Proper function name is how_many_imputations")
+  howManyImputations::how_many_imputations(model, cv, alpha)
 }
